18bf31312d68be2ea9681d5cb8379848bca161a9
SACHIN <sachinsaklani41@gmail.com>
2026-01-24 00:29:10 +0530

    Learned basics

---
+++ 800-1000-- Rating/2167A-Square.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin >> t;
+    for (int i=0;i<t;i++){
+        int a,b,c,d;
+        cin >> a>>b>>c>>d;
+        if (a==b&&b==c&&c==d) cout<<"YES"<< endl;
+        else cout<< "NO"<<endl;
+    }
+    return 0;
+}
