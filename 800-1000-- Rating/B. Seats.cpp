eb0d05eb0f10af448f35cdb17e25b443d75a8f01
SACHIN <sachinsaklani41@gmail.com>
2026-03-08 13:49:36 +0530

    Loigc--> Pading Trick For the edge 0s . Cout zeroes then Divide By 3.

---
+++ 800-1000-- Rating/B. Seats.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    string s;
+    cin >> s;
+    s = "1" + s + "1";
+
+    int ans = 0;
+
+    for (int i = 1, l = 0; i <= n; i++)
+    {
+        if (s[i] == '0')
+        {
+            if (s[i - 1] == '1')
+                l = i;   
+
+            if (s[i + 1] == '1')
+            {
+                int edge = (l == 1) + (i == n);
+                ans += (i - l + 1 + edge) / 3;
+            }
+        }
+        else
+        {
+            ans++; 
+        }
+    }
+
+    cout << ans << "\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--)
+        solve();
+
+    return 0;
+}
